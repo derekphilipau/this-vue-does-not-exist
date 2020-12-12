538e43c126ca3f48c96836d3cf1703c66da37bff
Derek Au <derek@derekau.net>
2020-12-12 08:43:26 -0700

    less videos on home page

--- src/views/VideoView.vue
+++ src/views/VideoView.vue
@@ -1,34 +1,24 @@
 <template>
   <div class="random-image" v-if="randomId">
     <div class="columns gallery-columns is-vcentered is-multiline">
-      <div class="column gallery-column is-one-third-widescreen is-half-desktop is-half-tablet">
+      <div class="column gallery-column is-half-widescreen is-half-desktop is-half-tablet">
         <video autoplay loop muted playsinline>
           <source src="https://thisvesseldoesnotexist.s3-us-west-2.amazonaws.com/public/videos/1024_random_204.mp4" type="video/mp4">
         </video>
       </div>
-      <div class="column gallery-column is-one-third-widescreen is-half-desktop is-half-tablet">
+      <div class="column gallery-column is-half-widescreen is-half-desktop is-half-tablet">
         <video autoplay loop muted playsinline>
           <source src="https://thisvesseldoesnotexist.s3-us-west-2.amazonaws.com/public/videos/1024_random_291.mp4" type="video/mp4">
         </video>
       </div>
-      <div class="column gallery-column is-one-third-widescreen is-half-desktop is-half-tablet">
+      <div class="column gallery-column is-half-widescreen is-half-desktop is-half-tablet">
         <video autoplay loop muted playsinline>
           <source src="https://thisvesseldoesnotexist.s3-us-west-2.amazonaws.com/public/videos/1024_random_293.mp4" type="video/mp4">
         </video>
       </div>
-      <div class="column gallery-column is-one-third-widescreen is-half-desktop is-half-tablet">
+      <div class="column gallery-column is-half-widescreen is-half-desktop is-half-tablet">
         <video autoplay loop muted playsinline>
           <source src="https://thisvesseldoesnotexist.s3-us-west-2.amazonaws.com/public/videos/1024_random_370.mp4" type="video/mp4">
-        </video>
-      </div>
-      <div class="column gallery-column is-one-third-widescreen is-half-desktop is-half-tablet">
-        <video autoplay loop muted playsinline>
-          <source src="https://thisvesseldoesnotexist.s3-us-west-2.amazonaws.com/public/videos/1024_random_466.mp4" type="video/mp4">
-        </video>
-      </div>
-      <div class="column gallery-column is-one-third-widescreen is-half-desktop is-half-tablet">
-        <video autoplay loop muted playsinline>
-          <source src="https://thisvesseldoesnotexist.s3-us-west-2.amazonaws.com/public/videos/1024_random_508.mp4" type="video/mp4">
         </video>
       </div>
     </div>
